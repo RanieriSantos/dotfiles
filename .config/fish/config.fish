dcb621ccb475398814622aa94895d663cc5c4a24
Ranieri Santos <ranierisantos@pm.me>
2021-05-16 10:15:07 -0300

    Fish config modification

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
 set fish_greeting
 
-alias dotfiles="/usr/bin/git --git-dir=$HOME/dotfiles --work-tree=$HOME"
+alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
 
 starship init fish | source
