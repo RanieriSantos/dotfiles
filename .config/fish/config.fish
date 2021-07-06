ee8fe98dd7b0d4773d0190c5abf1ab6a8fafe44c
Ranieri Santos <ranierisantos@pm.me>
2021-07-06 16:17:21 -0300

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 set fish_greeting
 
 alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
+alias pfetch="curl -s https://raw.githubusercontent.com/dylanaraps/pfetch/master/pfetch | sh"
 
 starship init fish | source
