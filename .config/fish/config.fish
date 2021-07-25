01a060ab3bd0759cf139795df4526e440b7f1ca8
Ranieri Santos <ranierisantos@pm.me>
2021-07-25 10:40:53 -0300

    Update fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,6 @@ set fish_greeting
 
 alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
 alias pfetch="curl -s https://raw.githubusercontent.com/dylanaraps/pfetch/master/pfetch | sh"
+alias grub-update="sudo grub2-mkconfig -o /boot/efi/EFI/redhat/grub.cfg"
 
 starship init fish | source
