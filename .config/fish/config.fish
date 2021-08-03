b47b29df3cc5841c70c41aa722dfcc0af4f82527
Ranieri Santos <ranierisantos@pm.me>
2021-08-03 19:34:34 -0300

    Update fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,12 @@
 set fish_greeting
 
+export PATH=/usr/bin:$PATH
+export DOCKER_HOST=unix:///run/user/1000/docker.sock
+
 alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
 alias pfetch="curl -s https://raw.githubusercontent.com/dylanaraps/pfetch/master/pfetch | sh"
-alias grub-update="sudo grub2-mkconfig -o /boot/efi/EFI/redhat/grub.cfg"
+alias grub-update="sudo grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg"
+alias sort-gnome-menu="gsettings reset org.gnome.shell app-picker-layout"
+alias install-starship="sh -c '(curl -fsSL https://starship.rs/install.sh)' | sh"
 
 starship init fish | source
